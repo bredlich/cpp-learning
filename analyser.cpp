b2954615131096cc9bad0c1502033c858b02cf4c
bredlich <bjr30@outlook.com>
2026-08-28 15:18:57 +0100

    Remove: accidental analyser.cpp file

--- analyser.cpp
+++
@@ -1,6 +0,0 @@
-Microsoft (R) Incremental Linker Version 14.51.36256.0
-Copyright (C) Microsoft Corporation.  All rights reserved.
-
-/out:csv.exe 
-csv 
-LINK : fatal error LNK1181: cannot open input file 'csv.obj'
